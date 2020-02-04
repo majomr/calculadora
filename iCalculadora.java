fce22f0e62d33a4171151a9a13b0546bea0b4386
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2020-02-03 20:54:45 -0600

    Update iCalculadora.java

--- iCalculadora.java
+++ iCalculadora.java
@@ -5,6 +5,15 @@ Hoja de trabajo 2
 Clase: interfaz calculadora
  */
 
-public interface iCalculadora{
+import java.io.IOException;
+import java.net.URISynaxException;
 
+public interface iCalculadora{
+  void init() throws IOException, URISyntaxException;
+  int sum(int a, int b);
+  int subtraction(int a, int b);
+  int division(int a, int b);
+  int multiply(int a, int b);
+  boolean isInt(String value);
+  void manageOperator(String operator);
 }
