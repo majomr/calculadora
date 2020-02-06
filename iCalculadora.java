eed78704edb4d911b52dd7dbeb6985c2c1dc8726
majomr <mor19145@uvg.edu.gt>
2020-02-05 20:40:56 -0600

    .

--- iCalculadora.java
+++ iCalculadora.java
@@ -5,15 +5,9 @@ Hoja de trabajo 2
 Clase: interfaz calculadora
  */
 
-import java.io.IOException;
-import java.net.URISynaxException;
-
 public interface iCalculadora{
-  void init() throws IOException, URISyntaxException;
-  int sum(int a, int b);
-  int subtraction(int a, int b);
-  int division(int a, int b);
-  int multiply(int a, int b);
-  boolean isInt(String value);
-  void manageOperator(String operator);
+  int sumar(int a, int b);
+  int restar(int a, int b);
+  int dividir(int a, int b);
+  int multiplicar(int a, int b);
 }
